20b44461575e15cfba58b2e074f1c879fe6f2b2e
Tyler Arehart <Tyler Arehart>
2025-04-27 19:52:16 -0700

    Updating pixel map to match new strand configuration

--- model/serpentine_pixel_map.py
+++ model/serpentine_pixel_map.py
@@ -5,13 +5,25 @@ class SerpentinePixelMap:
         self.serpentine = [[0] * width for _ in range(height)]
 
         led_index = 0
-        for col in range(width):
-            serpentine_range = range(height - 1, -1, -1) if col % 2 == 0 else range(height)
-            for row in serpentine_range:
+        for row in range(height-1, -1, -1):
+            if (height - 1 - row) % 2 == 0:
+                serpentine_range = range(width-1, -1, -1)  # right to left
+            else:
+                serpentine_range = range(width)  # left to right
+            for col in serpentine_range:
                 self.serpentine[row][col] = led_index
                 led_index += 1
 
         self.num_pixels = width * height
 
+        self.print_map()
+
     def get_pixel_index(self, row: int, col: int) -> int:
         return self.serpentine[row][col]
+
+
+    def print_map(self):
+        max_index = self.num_pixels - 1
+        width = len(str(max_index))  # Number of digits in the largest index
+        for row in self.serpentine:
+            print(" ".join(f"{idx:{width}}" for idx in row))
